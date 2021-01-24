9b97e4c7a9d2f1dc46fe8014a9a40153541a4433
vladimir.sukiasyan <vladimir.sukiasyan@corp.mail.ru>
2021-01-24 20:52:54 +0300

    Initial commit!

--- app/src/main/java/com/example/alexseyandroidlesson1/MainActivity.kt
+++ app/src/main/java/com/example/alexseyandroidlesson1/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.alexseyandroidlesson1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val editText = findViewById<EditText>(R.id.edit_text)
+        textView = findViewById(R.id.text_view)
+
+        editText.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val str = s.toString().toInt() + 1
+                textView.text = str.toString()
+            }
+
+        })
     }
 }
